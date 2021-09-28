3e79b026960d381444a6105087cb248fe17a1936
Guilherme Leite <61299435+leiteGuilherme@users.noreply.github.com>
2021-09-28 15:05:31 -0300

    variaveis e construtores/Funções para carregar o automato a partir do arquivo lido

--- lfa/src/services/Automaton.java
+++ lfa/src/services/Automaton.java
@@ -1,5 +1,36 @@
 package services;
 
 public class Automaton {
+    
+    private AutomatonReader automatonFile;
+    private ArrayList<State> states = new ArrayList<>();
+    private State initial;
+    private ArrayList<State> accepting = new ArrayList<>();
+    private ArrayList<String> alphabet = new ArrayList<>();
+    private ArrayList<Transition> transitions = new ArrayList<>();
+
+    public Automaton() {
+        this.automatonFile = new AutomatonReader();
+    }
+
+    
+    public void load() {
+        try {
+            this.initial = new State(automatonFile.getValues(Parameters.INITIAL).get(0).toString());
+            automatonFile.getValues(Parameters.STATES)
+                    .forEach(item -> this.states.add(new State(item.toString())));
+            automatonFile.getValues(Parameters.ACCEPTING)
+                    .forEach(item -> this.accepting.add(new State(item.toString())));
+            automatonFile.getValues(Parameters.ALPHABET)
+                    .forEach(item -> this.alphabet.add(item.toString()));
+
+            this.loadTransitions();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Erro na execução do algoritimo, verifique se os dados passados estão corretos!");
+        }
+    }
 
 }
