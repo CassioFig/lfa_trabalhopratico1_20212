e906bb612d172b3cca897808550db3449e484e6d
Guilherme Leite <61299435+leiteGuilherme@users.noreply.github.com>
2021-09-29 15:56:37 -0300

    Funções para carregar o automato a partir do arquivo lido pt2

--- lfa/src/services/Automaton.java
+++ lfa/src/services/Automaton.java
@@ -32,5 +32,23 @@ public class Automaton {
             System.out.println("Erro na execução do algoritimo, verifique se os dados passados estão corretos!");
         }
     }
+    private void loadTransitions() throws FileNotFoundException {
+        ArrayList<String> transitions_ = new ArrayList<>();
+        ArrayList<String> to = new ArrayList<>();
+        ArrayList<String> from = new ArrayList<>();
+        ArrayList<String> symbols = new ArrayList<>();
+
+        automatonFile.getValues(Parameters.TRANSITIONS).forEach(item -> transitions_.add(item.toString()));
+        transitions_.forEach(item -> to.add(item.split(":")[0]));
+        transitions_.forEach(item -> symbols.add(item.split(":")[1].split(">")[0]));
+        transitions_.forEach(item -> from.add(item.split(":")[1].split(">")[1]));
+
+        for (int i = 0; i < transitions_.toArray().length; i++) {
+            State to_ = new State(to.get(i));
+            State from_ = new State(from.get(i));
+
+            this.transitions.add(new Transition(to_, from_, symbols.get(i)));
+        }
+    }
 
 }
